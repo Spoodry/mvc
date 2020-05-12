7ee71084591ab7e0a22e76010c62aed87438e472
Spoodry <jan080599xd@gmail.com>
2020-05-12 17:58:23 -0500

    primer commit

---
+++ controllers/error.php
@@ -0,0 +1,11 @@
+<?php
+
+    class MyError extends Controller{
+        function __construct() {
+            parent::__construct();
+            $this->view->mensaje = "Error al cargar el recurso";
+            $this->view->render('error/index');
+        }
+    }
+
+?>
